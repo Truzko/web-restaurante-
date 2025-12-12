fea1aaaa70e0ffc35efea358a58f66c57d61b7a2
Truzko <maxitruzko06@gmail.com>
2025-12-11 22:45:00 -0300

    Limpieza total: dejo solo la carpeta TP10 Integración/TP8 SUSHIRU

--- TP11ObraSocial/php/1logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Iniciar la sesión
-session_unset(); // Eliminar todas las variables de sesión
-session_destroy(); // Destruir la sesión
-
-// Redirigir al usuario a la página de inicio de sesión
-header("Location: ../html/index.html");
-exit();
-?>
--- TP7 Pactica/ejer6/java.js
+++
@@ -1,9 +0,0 @@
-function calcular() {
-    var numero1 = parseFloat(document.getElementById('numero1').value);
-    let array = [0,1];
-    for(let i = 0 ; i < numero1 ; i++){
-        array.push(array[i] +  array[i+1]);
-    document.getElementById('figo').innerText = array[i];
-    console.log(array[i]);
-    }
-}
